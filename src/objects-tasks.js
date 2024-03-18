5a9be489d1691b0c49290dc7b82214c7705a3cd3
Igor Moskalev <moskalev.igor.v@gmail.com>
2024-03-19 01:17:14 +0500

    feat: implement removeProperties, compareObjects, isEmptyObject, makeImmutable functions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -59,8 +59,20 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  let keysArr = keys;
+  const tmpObj = obj;
+  if (!Array.isArray(keys)) {
+    keysArr = [keys];
+  }
+
+  keysArr.forEach((key) => {
+    if (key in tmpObj) {
+      delete tmpObj[key];
+    }
+  });
+
+  return tmpObj;
 }
 
 /**
@@ -75,8 +87,8 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -90,8 +102,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -110,8 +122,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
